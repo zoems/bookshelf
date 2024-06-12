ca165b1056e2f9ae0aa0f7b8b4725d8759e69ed1
Zoe Mawby Smart <zoemsmart@gmail.com>
2024-06-12 12:20:49 +0200

    feat: auth api async

--- src/utils/api-client.exercise.js
+++ src/utils/api-client.exercise.js
@@ -1,12 +1,26 @@
+import * as auth from 'auth-provider'
 const apiURL = process.env.REACT_APP_API_URL
 
-function client(endpoint, customConfig = {}) {
+function client(
+  endpoint,
+  {token, headers: customHeaders, ...customConfig} = {},
+) {
   const config = {
     method: 'GET',
+    headers: {
+      Authorization: token ? `Bearer ${token}` : undefined,
+      ...customHeaders,
+    },
     ...customConfig,
   }
 
   return window.fetch(`${apiURL}/${endpoint}`, config).then(async response => {
+    if (response.status === 401) {
+      await auth.logout()
+      // refresh the page for them
+      window.location.assign(window.location)
+      return Promise.reject({message: 'Please re-authenticate.'})
+    }
     const data = await response.json()
     if (response.ok) {
       return data
